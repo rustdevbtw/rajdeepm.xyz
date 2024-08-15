3b7f022237d0d9226a7fe2895f411d37a8cb7311
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2024-08-15 23:31:41 +0530

    add some html sprinkles
    Signed-off-by: Rajdeep Malakar <rajdeepm.dev@gmail.com>

--- main.ts
+++ main.ts
@@ -1,74 +1,100 @@
-import { Hono } from "@hono/hono";
-import { serveStatic } from "@hono/hono/deno";
+import { Context, Hono } from "@hono/hono";
 import posts from "./meta.json" with { type: "json" };
 
 const app = new Hono();
 
-async function sstatic(p: string, headers: unknown = {}): Response {
-  return new Response(await Deno.readTextFile(p), {
+async function sstatic(p: string, headers: object = {}): Promise<Response> {
+  return new Response(await Deno.readFile(p), {
     headers: {
-      "X-Is-Caveman": 1,
+      "X-Is-Caveman": "1",
       "X-File": p,
-      ...headers
-    }
+      ...headers,
+    },
   });
 }
 
-const BASE: string = "https://raw.githubusercontent.com/rustdevbtw/rajdeepm.xyz/%S/%F";
+const BASE: string =
+  "https://raw.githubusercontent.com/rustdevbtw/rajdeepm.xyz/%S/%F";
 
-async function fstatic(sha: string, p: string, headers: unknown = {}): Response {
+async function fstatic(
+  sha: string,
+  p: string,
+  headers: object = {},
+): Promise<Response> {
   const r = BASE.replaceAll("%S", sha).replaceAll("/%F", p);
-  return new Response(await (await fetch(r)).text(), {
+  return new Response(await (await fetch(r)).blob(), {
     headers: {
-      "X-Is-Caveman": 1,
+      "X-Is-Caveman": "1",
       "X-File": r,
-      "X-Is-Archive": 1,
+      "X-Is-Archive": "1",
       "X-SHA": sha,
-      ...headers
-    }
-  })
+      ...headers,
+    },
+  });
 }
 
-app.get("*", async c => {
+app.get("*", async (c: Context) => {
   const sha = c.req.query("sha") || c.req.header("X-Archive-SHA") || false;
-  const latest = c.req.query("latest") != undefined || c.req.header("X-Archive-Latest") || false;
+  const latest = c.req.query("latest") != undefined ||
+    c.req.header("X-Archive-Latest") || false;
+  const html = c.req.query("html") != undefined ||
+    c.req.header("Accept")?.includes("text/html") || c.req.header("X-HTML") ||
+    false;
   if (!sha && latest) {
     switch (c.req.path) {
-      case '/':
-        return sstatic("index.txt");
-      case '/gpg':
+      case "/":
+        return sstatic(`index.${html ? "html" : "txt"}`, {
+          "Content-Type": html ? "text/html" : "text/plain",
+        });
+      case "/gpg":
         return sstatic("./keys/gpg");
-      case '/ssh':
+      case "/ssh":
         return sstatic("./keys/ssh");
-      case '/favicon.ico':
-        return sstatic("./favicon.png", { "Content-Type": "image/png"});
+      case "/favicon.ico":
+        return sstatic("./favicon.png", { "Content-Type": "image/png" });
+      case "/font.woff2":
+        return sstatic("/font.woff2", { "Content-Type": "font/woff2" });
       default:
         try {
-          return await sstatic(`./posts${c.req.path}.txt`);
+          return await sstatic(
+            `./posts${c.req.path}.${html ? "html" : "txt"}`,
+            { "Content-Type": html ? "text/html" : "text/plain" },
+          );
         } catch (err) {
           return new Response(`Woopsie!\nSomething went wrong:\n${err}`);
         }
     }
   } else {
-    if (!sha) return c.redirect(`${c.req.path}?sha=${posts["SHA"]}`);
+    if (!sha) {
+      // @ts-ignore posts has everything. Don't worry
+      return c.redirect(`${c.req.path}?sha=${posts["SHA"]}`);
+    }
 
     switch (c.req.path) {
-      case '/':
-        return fstatic(sha, "/index.txt");
-      case '/gpg':
-        return fstatic(sha, "/keys/gpg");
-      case '/ssh':
-        return fstatic(sha, "/keys/ssh");
-      case '/favicon.ico':
-        return fstatic(sha, "/favicon.png", { "Content-Type": "image/png", "X-Is-ICO": 0 });
+      case "/":
+        return fstatic(sha, `index.${html ? "html" : "txt"}`, {
+          headers: { "Content-Type": html ? "text/html" : "text/plain" },
+        });
+      case "/gpg":
+        return fstatic(sha, "./keys/gpg");
+      case "/ssh":
+        return fstatic(sha, "./keys/ssh");
+      case "/favicon.ico":
+        return fstatic(sha, "./favicon.png", { "Content-Type": "image/png" });
+      case "/font.woff2":
+        return fstatic(sha, "/font.woff2", { "Content-Type": "font/woff2" });
       default:
         try {
-          return await fstatic(sha, `/posts${c.req.path}.txt`);
+          return await fstatic(
+            sha,
+            `./posts${c.req.path}.${html ? "html" : "txt"}`,
+            { "Content-Type": html ? "text/html" : "text/plain" },
+          );
         } catch (err) {
           return new Response(`Woopsie!\nSomething went wrong:\n${err}`);
         }
     }
   }
-})
+});
 
 Deno.serve(app.fetch);
